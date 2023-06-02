9645c5c0f1ad1e313dcc87cd5aa64505dae4191c
Mattia Giuffrida <giuffrida.mattia@gmail.com>
2023-06-02 17:13:44 +0100

    Introduce new flow_typing static tc tests.
    These will be used to test sorbet's ability to run Flow-Sensitive Typing with `Typed::Result`

---
+++ test/test_data/flow_typing.rb
@@ -0,0 +1,25 @@
+# typed: true
+# frozen_string_literal: true
+
+class TestGenerics
+  extend T::Sig
+
+  sig { params(should_succeed: T::Boolean).returns(Typed::Result[Integer, String]) }
+  def do_something(should_succeed)
+    if should_succeed
+      Typed::Success.new(payload: 123)
+    else
+      Typed::Failure.new(error: "")
+    end
+  end
+
+  sig { void }
+  def test_flow
+    result = do_something(true)
+    if result.success?
+      T.assert_type!(result.payload, Integer)
+    else
+      T.assert_type!(result.error, String)
+    end
+  end
+end
