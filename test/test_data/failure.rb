7c001ee22f61d1c3b5cf88d10026285ca50aee58
Mattia Giuffrida <giuffrida.mattia@gmail.com>
2023-06-02 17:12:51 +0100

    Update success and add failure static tc tests.

---
+++ test/test_data/failure.rb
@@ -0,0 +1,28 @@
+# typed: strict
+# frozen_string_literal: true
+
+class TestGenerics
+  extend T::Sig
+
+  sig { params(should_succeed: T::Boolean).returns(Typed::Result[Integer, String]) }
+  def test_generic_initializer(should_succeed)
+    if should_succeed
+      Typed::Success.new(payload: 1)
+    else
+      Typed::Failure.new(error: 1)
+    end
+  end
+
+  sig { returns(Typed::Failure[String]) }
+  def test_inferred_error_type
+    failure = Typed::Failure.new(error: "error")
+
+    T.assert_type!(failure.error, String)
+    failure
+  end
+
+  sig { returns(Typed::Failure[Integer]) }
+  def test_explicit_error_type
+    Typed::Failure[Integer].new(error: "error")
+  end
+end
--- test/test_data/success.rb
+++ test/test_data/success.rb
@@ -5,11 +5,24 @@ class TestGenerics
   extend T::Sig
 
   sig { params(should_succeed: T::Boolean).returns(Typed::Result[Integer, String]) }
-  def test(should_succeed)
+  def test_generic_initializer(should_succeed)
     if should_succeed
       Typed::Success.new(payload: "1")
     else
       Typed::Failure.new(error: "")
     end
+  end
+
+  sig { returns(Typed::Success[String]) }
+  def test_inferred_payload_type
+    success = Typed::Success.new(payload: "success")
+
+    T.assert_type!(success.payload, String)
+    success
+  end
+
+  sig { returns(Typed::Success[Integer]) }
+  def test_explicit_payload_type
+    Typed::Success[Integer].new(payload: "success")
   end
 end
