212dd18609b1fffff980e4bc050b73186a3a7c76
Max VelDink <maxveldink@gmail.com>
2023-06-20 08:59:18 -0400

    Update rubocop disables

--- lib/typed/success.rb
+++ lib/typed/success.rb
@@ -60,20 +60,20 @@ module Typed
 
     sig do
       override
-        .params(blk: T.proc.params(arg0: Error).void)
+        .params(_blk: T.proc.params(arg0: Error).void)
         .returns(T.self_type)
     end
-    def on_error(&blk) # rubocop:disable Lint/UnusedMethodArgument
+    def on_error(&_blk)
       self
     end
 
     sig do
       override
         .type_parameters(:Fallback)
-        .params(value: T.type_parameter(:Fallback))
+        .params(_value: T.type_parameter(:Fallback))
         .returns(T.any(Payload, T.type_parameter(:Fallback)))
     end
-    def payload_or(value) # rubocop:disable Lint/UnusedMethodArgument
+    def payload_or(_value)
       payload
     end
   end
